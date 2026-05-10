79fb750142958f9f5bdf115151603855333514c3
Muhammad Areeb Shahzad <132228071+mainareeb@users.noreply.github.com>
2026-05-10 09:13:33 +0500

    Update Canada image URL in Home.js

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -366,7 +366,7 @@ function Home() {
     {
       name: "Canada",
       flag: "🇨🇦",
-      img: "https://images.unsplash.com/photo-1569982175971-d92b01cf8694?w=400&q=80",
+      img: "https://plus.unsplash.com/premium_photo-1673241100156-2e04fca1a4af?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dl",
       country: "Canada",
     },
     {
